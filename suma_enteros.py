7092f9ed96374857f924ad082170ebbe2eceea82
sistemas <adrianforero@colegioguanenta.edu.co>
2023-04-28 10:09:44 -0500

    alo

--- suma_enteros.py
+++ suma_enteros.py
@@ -4,10 +4,22 @@
 
 # se importa la libreria tkinter con todas sus funciones 
 from tkinter import *
-
+from tkinter import messagebox
 #----------------------------
 # funciones de la app
 #----------------------------
+
+#sumar
+def sumar():
+    pass
+#borrar
+def borrar():
+    pass
+
+#salir
+def salir():
+    messagebox.showinfo("Suma Enteros 1.0", "La app se va a cerrar")
+    ventana_principal.destroy()
 
 #-----------------------------
 # ventana principal de la app
@@ -28,6 +40,12 @@ ventana_principal.resizable(False, False)
 # COLOR DE FONDO DE LA VENTANA 
 ventana_principal.config(bg="green")
 
+#----------------------------
+# variables globales
+#----------------------------
+x = StringVar()
+y = StringVar() 
+
 #-------------------------
 # FRAMA ENTRADA DATOS
 #-------------------------
@@ -35,15 +53,35 @@ frame_entrada = Frame(ventana_principal)
 frame_entrada.config(bg="white", width=480, height=180)
 frame_entrada.place(x=10, y=10)
 
-
 # LOGO DE LA APP 
 logo = PhotoImage(file="IMG/descarga.png")
 lb_logo = Label(frame_entrada, image=logo, bg="white")
 lb_logo.place(x=0,y=10)
 
+# etiqueta para el valor de x 
+lb_x = Label(frame_entrada, text = "X = ")
+lb_x.config(bg="white", fg="blue", font=("Helvetica", 18))
+lb_x.place(x=240, y=60)
+
+# caja de texto para valor x
+entry_x = Entry(frame_entrada)
+entry_x.config(bg="white", fg="blue", font=("Times New Roman", 18), width=6)
+entry_x.focus_set()
+entry_x.place(x=290,y=60)
+
+# etiqueta para el valor de y
+lb_x = Label(frame_entrada, text = "Y = ")
+lb_x.config(bg="white", fg="blue", font=("Helvetica", 18))
+lb_x.place(x=240, y=120)
+
+# caja de texto para valor y
+entry_y = Entry(frame_entrada, textvariable=y)
+entry_y.config(bg="white", fg="blue", font=("Times New Roman", 18), width=6)
+entry_y.focus_set()
+entry_y.place(x=290,y=120)
 
 # TITULO DE APP 
-titulo = Label(frame_entrada, text="suma_enteros_1.0")
+titulo = Label(frame_entrada, text="suma enteros 1.0")
 titulo.config(bg="green", fg="white", font=("Helvetica", 16))
 titulo.place(x=240,y=10)
 
@@ -56,12 +94,29 @@ frame_operaciones = Frame(ventana_principal)
 frame_operaciones.config(bg="white", width=480, height=100)
 frame_operaciones.place(x=10, y=200)
 
+# boton para sumar 
+bl_sumar = Button(frame_operaciones,text="Sumar", command=sumar)
+bl_sumar.place(x=45, y=35, width=100, height=30)
+
+# boton para borrar
+bt_borrar = Button(frame_operaciones, text="Borrar", command=borrar)
+bt_borrar.place(x=190, y=35, width=100, height=30)
+
+# boton para salir
+bt_salir = Button(frame_operaciones, text="Salir", command=salir)
+bt_salir.place(x=335, y=35, width=100, height=30)
+
 #-------------------------
 # FRAMA RESULTADOS
 #-------------------------
 frame_resultados = Frame(ventana_principal)
 frame_resultados.config(bg="white", width=480, height=180)
 frame_resultados.place(x=10, y=310)
+
+# are del texto de los resultados
+t_resultados = Text(frame_resultados)
+t_resultados.config(bg="black", fg="green yellow", font=("Courier, 20"))
+t_resultados.place(x=10,y=10,width=460,height=160)
 
 # run 
 # SE EJECUTA EL METODO MAINLOOP() DE LA CLASE TK() A TRAVES DE LA INSTANCIA DE LA INTANCIA ventana_principal. Este metodo despliega la ventana en pantalla y queda a la espera de le que el usuario haga (click en un boton, escribir, etc). Cada accion del usuario se conoce como unn evento. El método mainloop() es un bucle infinito.
